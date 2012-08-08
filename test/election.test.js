542e73dca80fcf340f8d70742371214e6efd7a9c
Yunong Xiao <yunong@joyent.com>
2012-08-08 14:15:33 -0700

    make explicit election path optional, add tests.

--- test/election.test.js
+++ test/election.test.js
@@ -19,7 +19,7 @@ var test = helper.test;
 
 var LOG = helper.createLogger('election.test.js');
 var DIR_PATH = '/' + uuid().substr(0, 7);
-var PATH = DIR_PATH + '/' + uuid().substr(0, 7);
+var PATH = uuid().substr(0, 7);
 var ZK;
 
 
@@ -111,7 +111,79 @@ test('election', function (t) {
         for (var i = 0; i < 3; i++) {
                 voters.push(zk.createElection({
                         client: ZK,
-                        path: PATH,
+                        path: DIR_PATH,
+                        log: LOG,
+                        object: {
+                                node: i
+                        }
+                }));
+        }
+
+        var leaderIndex;
+        voters.forEach(function (v, index) {
+                v.vote(function (err, isLeader) {
+                        t.ifError(err);
+                        if (isLeader) {
+                                t.equal(leader, null);
+                                leader = voters[index];
+                                leaderIndex = index;
+                        }
+
+                        if (++ready === 3) {
+                                t.notEqual(leader, null);
+                                voters.splice(leaderIndex, 1);
+                                watch();
+                        }
+                });
+        });
+});
+
+
+test('election with prefix', function (t) {
+        var leader = null;
+        var ready = 0;
+        var voters = [];
+
+        function watch() {
+                var watching = 0;
+                voters.forEach(function (v) {
+                        v.watch(function (err) {
+                                t.ifError(err);
+                                if (++watching === voters.length)
+                                        newLeader();
+                        });
+                });
+        }
+
+        function newLeader() {
+                // The logic below acts to have leaders commit seppuku
+                // in order
+                var newLeaderSeen = 0;
+                var stopped = 0;
+                voters.forEach(function (v) {
+                        v.on('leader', function () {
+                                v.on('close', function () {
+                                        if (++stopped === voters.length) {
+                                                t.equal(newLeaderSeen, 1);
+                                                t.end();
+                                        }
+                                });
+                                v.stop();
+                        });
+
+                        v.on('newLeader', function (l) {
+                                newLeaderSeen++;
+                                t.ok(l);
+                        });
+                });
+                leader.stop();
+        }
+
+        for (var i = 0; i < 3; i++) {
+                voters.push(zk.createElection({
+                        client: ZK,
+                        path: DIR_PATH,
+                        pathPrefix: PATH,
                         log: LOG,
                         object: {
                                 node: i
