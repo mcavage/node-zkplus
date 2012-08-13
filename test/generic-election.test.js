c1696afc102f53b80d3000177d59e6a5bdb4033e
Yunong Xiao <yunong@joyent.com>
2012-08-13 14:26:20 -0700

    add generic-election which adheres to the zk election recipe. client.js emits delete event if a watch returns ENONODE error if the watch has already fired once. this catches the rare case where the znode gets deleted before the re-watch can be added

--- test/generic-election.test.js
+++ test/generic-election.test.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 var uuid = require('node-uuid');
-
+var vasync = require('vasync');
 var zk = require('../lib');
 
 if (require.cache[__dirname + '/helper.js'])
@@ -30,7 +30,7 @@ var VOTER3;
 
 ///--- Tests
 
-before(function (callback) {
+test('beforeClass', function(t) {
         try {
                 ZK = zk.createClient({
                         log: LOG,
@@ -52,38 +52,311 @@ before(function (callback) {
                                 process.exit(1);
                         }
 
-                        callback();
+                        t.end();
                 });
         });
 });
 
-
-after(function (callback) {
-        LOG.trace({path: DIR_PATH}, 'after: cleaning up');
-        ZK.rmr(DIR_PATH, function (err) {
-                if (err) {
-                        console.error('Unable to clean up %s', DIR_PATH);
-                        process.exit(1);
-                }
-                ZK.on('close', callback);
-                ZK.close();
+/**
+ * Initial test, test that having 3 voters results in the expected chain of
+ * leadership. e.g. 1<-2<-3
+ */
+test('reset state', function(t) {
+        _resetState(function(err) {
+                t.ifError(err);
+                t.end();
         });
 });
 
-test('create voters', function(t) {
+test('check voters', function(t) {
+        t.ok(VOTER1.amLeader);
+        t.notOk(VOTER2.amLeader);
+        t.notOk(VOTER3.amLeader);
+        t.equal(VOTER1.path.split('-')[1], VOTER2.leader.split('-')[1]);
+        t.equal(VOTER2.path.split('-')[1], VOTER3.leader.split('-')[1]);
+        t.equal();
+        t.equal();
+        t.end();
+});
 
+/**
+ * Test the serial removal of v1, v2 and the addition of v1, v2.
+ */
+test('reset state' + uuid().substr(0,7), function(t) {
+        // append uuid because nodeunit can't handle test funcs with the same
+        // name.
+        _resetState(function(err) {
+                t.ifError(err);
+                t.end();
+        });
+});
+
+test('remove v1', function(t) {
+        VOTER1.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('leader', function() {
+                t.end();
+        });
+
+        VOTER2.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('leader', function() {
+        });
+
+        VOTER3.once('newLeader', function(leader) {
+                t.ifError(true);
+                t.end();
+        });
+
+        LOG.info('stopping voter1');
+        VOTER1.stop();
+});
+
+test('remove v2', function(t) {
+        VOTER1.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('leader', function() {
+                t.end();
+        });
+
+        VOTER3.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.stop();
+});
+
+test('add v1 back', function(t) {
+        VOTER1.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.once('newLeader', function(leader) {
+                t.equal(VOTER3.path.split('-')[1], leader.split('-')[1]);
+                t.end();
+        });
+
+        VOTER2.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.vote(function(err) {
+                if (err) {
+                        t.ifError(err);
+                        t.end();
+                }
+        });
+});
+
+test('add v2 back', function(t) {
+        VOTER1.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.once('newLeader', function(leader) {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('newLeader', function(leader) {
+                t.equal(VOTER1.path.split('-')[1], leader.split('-')[1]);
+                t.end();
+        });
+
+        VOTER3.once('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.vote(function(err) {
+                if (err) {
+                        t.ifError(err);
+                        t.end();
+                }
+        });
+});
+
+/**
+ * Test removing the middle node in the election chain.
+ */
+test('reset state' + uuid().substr(0,7), function(t) {
+        _resetState(function(err) {
+                t.ifError(err);
+                LOG.info('finished resetting state');
+                t.end();
+        });
+});
+
+test('remove v2 ' + uuid().substr(0,7), function(t) {
+        VOTER1.once('leader', function() {
+                LOG.error('emitter shouldn\'t fire');
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.once('newLeader', function(leader) {
+                LOG.error('emitter shouldn\'t fire');
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('leader', function() {
+                LOG.error('emitter shouldn\'t fire');
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.once('newLeader', function(leader) {
+                LOG.error('emitter shouldn\'t fire');
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('leader', function() {
+                LOG.error('emitter shouldn\'t fire');
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.once('newLeader', function(leader) {
+                t.equal(VOTER1.path.split('-')[1], leader.split('-')[1]);
+                t.end();
+        });
+
+        VOTER2.stop();
+});
+
+/**
+ * Test removing the middle node and adding it back.
+ */
+test('reset state remove v2 ' + uuid().substr(0,7), function(t) {
+        _resetState(function(err) {
+                t.ifError(err);
+                LOG.info('finished resetting state');
+                t.end();
+        });
+});
+
+test('add v2 ' + uuid().substr(0,7), function(t) {
+        VOTER1.on('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER1.on('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.on('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.on('newLeader', function(leader) {
+                t.equal(VOTER3.path.split('-')[1], leader.split('-')[1]);
+                t.end();
+        });
+
+        VOTER3.on('leader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER3.on('newLeader', function() {
+                t.ifError(true);
+                t.end();
+        });
+
+        VOTER2.vote(function(err) {
+                if (err) {
+                        t.ifError(err);
+                        t.end();
+                }
+        });
+});
+
+test('afterclass', function(t) {
+        LOG.info({path: DIR_PATH}, 'after: cleaning up');
+        ZK.on('close', function() {
+                t.end();
+        });
+        ZK.close();
+});
+
+function _resetState(callback) {
         VOTER1 = zk.createGenericElection({
                 client: ZK,
                 path: DIR_PATH,
                 log: LOG,
                 object: {}
         });
+
         VOTER2 = zk.createGenericElection({
                 client: ZK,
                 path: DIR_PATH,
                 log: LOG,
                 object: {}
         });
+
         VOTER3 = zk.createGenericElection({
                 client: ZK,
                 path: DIR_PATH,
@@ -91,156 +364,85 @@ test('create voters', function(t) {
                 object: {}
         });
 
-        t.done();
-});
-
-test('elect v1', function(t) {
-        VOTER1.on('leader', function() {
-                LOG.error('i am leader %s', voter.znode);
-                t.done();
-        });
-
-        VOTER1.on('newLeader', function(leader) {
-                LOG.error('should not be newleader');
-                var error = new Error('node should never be leader');
-                t.ifError(error);
-                t.done();
-        });
-
-        VOTER1.vote(function(err) {
-                t.ifError(err);
-                if (err) {
-                        t.done();
+        var tasks = [
+                function closeZK(_, cb) {
+                        ZK.close();
+                        cb();
+                },
+                function newZk(_, cb) {
+                        resetZK(cb);
+                },
+                function createV1(_, cb) {
+                        VOTER1 = zk.createGenericElection({
+                                client: ZK,
+                                path: DIR_PATH,
+                                log: LOG,
+                                object: {}
+                        });
+                        cb();
+                },
+                function createV2(_, cb) {
+                        VOTER2 = zk.createGenericElection({
+                                client: ZK,
+                                path: DIR_PATH,
+                                log: LOG,
+                                object: {}
+                        });
+                        cb();
+                },
+                function createV3(_, cb) {
+                        VOTER3 = zk.createGenericElection({
+                                client: ZK,
+                                path: DIR_PATH,
+                                log: LOG,
+                                object: {}
+                        });
+                        cb();
+                },
+                function vote1(_, cb) {
+                        VOTER1.vote(cb);
+                },
+                function vote2(_, cb) {
+                        VOTER2.vote(cb);
+                },
+                function vote3(_, cb) {
+                        VOTER3.vote(cb);
                 }
-                LOG.error('voted');
+        ];
+
+        vasync.pipeline({funcs: tasks}, function (err) {
+                if (err) {
+                        LOG.error('error resetting state');
+                }
+                return callback(err);
         });
-});
 
-//test('election', function(t) {
-        //var voter = zk.createGenericElection({
-                //client: zk.createClient({
-                        //log: LOG,
-                        //servers: [ {
-                                //host: (process.env.ZK_HOST ||
-                                //'localhost'),
-                                //port: (process.env.ZK_PORT || 2181)
-                        //}],
-                        //timeout: 1000
-                //}),
-                //path: DIR_PATH,
-                //log: LOG,
-                //object: {}
-        //});
+        function resetZK(callback) {
+                try {
+                        ZK = zk.createClient({
+                                log: LOG,
+                                servers: [ {
+                                        host: (process.env.ZK_HOST ||
+                                               'localhost'),
+                                        port: (process.env.ZK_PORT || 2181)
+                                }],
+                                timeout: 1000
+                        });
+                } catch (e) {
+                        console.error(e.stack);
+                        process.exit(1);
+                }
 
-        //voter.on('error', function(err) {
-                //t.ifError(err);
-        //});
+                ZK.on('connect', function () {
+                        ZK.mkdirp(DIR_PATH, function (err) {
+                                if (err) {
+                                        console.error(err.stack);
+                                        process.exit(1);
+                                }
 
-        //var v1Leader = false;
-        //voter.on('leader', function() {
-                //v1Leader = true;
-                //LOG.error('i am leader %s', voter.znode);
-        //});
+                                callback();
+                        });
+                });
+        }
 
-        //voter.on('newLeader', function(leader) {
-                //var error = new Error('node should never be leader');
-                //t.ifError(error);
-        //});
-
-        //// second voter
-        //var voter2 = zk.createGenericElection({
-                //client: zk.createClient({
-                        //log: LOG,
-                        //servers: [ {
-                                //host: (process.env.ZK_HOST ||
-                                //'localhost'),
-                                //port: (process.env.ZK_PORT || 2181)
-                        //}],
-                        //timeout: 1000
-                //}),
-                //path: DIR_PATH,
-                //log: LOG,
-                //object: {}
-        //});
-
-        //voter2.on('error', function(err) {
-                //t.ifError(err);
-        //});
-
-        //var v2Leader = false;
-        //voter2.on('leader', function() {
-                //LOG.error('i am leader %s', voter2.znode);
-                //v2Leader = true;
-                //t.ok(v1Leader);
-
-        //});
-
-        //var v2NewLeader = false;
-        //voter2.on('newLeader', function(leader) {
-                //LOG.error('i am %s my new leader is %s', voter2.znode, leader);
-                //t.equal(leader, '-0000000000');
-                //t.equal(false, v2NewLeader);
-                //v2NewLeader = true;
-        //});
-
-        //// third voter
-        //var voter3 = zk.createGenericElection({
-                //client: zk.createClient({
-                        //log: LOG,
-                        //servers: [ {
-                                //host: (process.env.ZK_HOST ||
-                                //'localhost'),
-                                //port: (process.env.ZK_PORT || 2181)
-                        //}],
-                        //timeout: 1000
-                //}),
-                //path: DIR_PATH,
-                //log: LOG,
-                //object: {}
-        //});
-
-        //voter3.on('error', function(err) {
-                //t.ifError(err);
-        //});
-
-        //var v3Leader = false;
-        //voter3.on('leader', function() {
-                //LOG.error('i am leader %s', voter3.znode);
-                //t.equal(false, v3Leader);
-                //t.ok(v1Leader);
-                //t.ok(v2Leader);
-                //v3Leader = true;
-                //t.end();
-        //});
-
-        //var v3NewLeader = false;
-        //voter3.on('newLeader', function(leader) {
-                //LOG.error('i am %s my new leader is %s', voter3.znode, leader);
-                //t.equal(leader, '-0000000001');
-                //v3NewLeader = true;
-        //});
-
-        //voter.vote(function(err) {
-                //t.ifError(err);
-                //if (err) {
-                        //LOG.error({err:err});
-                        //process.exit(1);
-                //}
-        //});
-
-        //voter2.vote(function(err) {
-                //t.ifError(err);
-                //if (err) {
-                        //LOG.error({err:err});
-                        //process.exit(1);
-                //}
-        //});
-
-        //voter3.vote(function(err) {
-                //t.ifError(err);
-                //if (err) {
-                        //LOG.error({err:err});
-                        //process.exit(1);
-                //}
-        //});
-//});
+}
